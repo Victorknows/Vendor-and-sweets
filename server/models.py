54c01a5e47f8490274dd21c19a3ab7da14aad988
victor <victormuhoro7@gmail.com>
2024-04-15 08:30:55 +0300

    Done

--- server/models.py
+++ server/models.py
@@ -9,47 +9,56 @@ metadata = MetaData(naming_convention={
 })
 
 db = SQLAlchemy(metadata=metadata)
-
-
+       
 class Sweet(db.Model, SerializerMixin):
     __tablename__ = 'sweets'
-
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
+    #Relationship
+    vendor_sweets = db.relationship('VendorSweet', back_populates='sweet', cascade="all, delete")
+    #Serialization
+    def serialize(self):
+        return {"id": self.id, "name": self.name}
 
-    # Add relationship
-    
-    # Add serialization
-    
     def __repr__(self):
-        return f'<Sweet {self.id}>'
-
+        return f'<Sweet id={self.id} name={self.name}>'
 
 class Vendor(db.Model, SerializerMixin):
     __tablename__ = 'vendors'
-
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
+    #Relationship
+    vendor_sweets = db.relationship('VendorSweet', back_populates='vendor', cascade="all, delete")
+    #Serialization
+    def serialize(self):
+        return {"id": self.id, "name": self.name}
 
-    # Add relationship
-    
-    # Add serialization
-    
     def __repr__(self):
-        return f'<Vendor {self.id}>'
-
-
+        return f'<Vendor id={self.id} name={self.name}>'  
+    
 class VendorSweet(db.Model, SerializerMixin):
     __tablename__ = 'vendor_sweets'
-
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
+    sweet_id = db.Column(db.Integer, db.ForeignKey('sweets.id'), nullable=False)
+    vendor_id = db.Column(db.Integer, db.ForeignKey('vendors.id'), nullable=False)
 
-    # Add relationships
-    
-    # Add serialization
-    
-    # Add validation
-    
+   #Relationship
+    sweet = db.relationship('Sweet', back_populates='vendor_sweets')
+    vendor = db.relationship('Vendor', back_populates='vendor_sweets')
+
+    #Serialization
+    def serialize(self):
+        return {"id": self.id, "price": self.price, "vendor_id": self.vendor_id, "sweet_id": self.sweet_id}
+
+    #Validation
+    @validates('price')
+    def validate_price(self, key, price):
+        if price is None:
+            raise ValueError('Price must have a value')
+        if price < 0:
+            raise ValueError("Price must not be a negative value")
+        return price 
+
     def __repr__(self):
-        return f'<VendorSweet {self.id}>'
+         return f'<VendorSweet id={self.id} price={self.price}, vendor_id={self.vendor_id} sweet_id={self.sweet_id}>'
